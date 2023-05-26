3ace7ac77c500d853aa0216606251befbe40b682
Li-Qing Wang <liqingnz@gmail.com>
2023-05-26 17:19:32 +0800

    test version

---
+++ contracts/FreePass.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+contract FreePass is ERC721Enumerable, Ownable {
+    struct MintEvent {
+        address token;
+        uint256 price;
+        uint256 startTime;
+        uint256 endTime;
+        bytes32 merkleRoot;
+        mapping(address => bool) mintedAddresses;
+    }
+
+    uint256 public eventIndex;
+    uint256 public tokenIndex;
+    mapping(uint256 => MintEvent) public mintEvents;
+
+    event EventCreated(
+        uint256 indexed index,
+        address indexed token,
+        uint256 startTime,
+        uint256 endTime,
+        bytes32 merkleRoot
+    );
+    event Minted(uint256 indexed index, address indexed claimer);
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+    function setupEvent(
+        address _token,
+        uint256 _price,
+        uint256 _endTimeFromNow,
+        bytes32 _merkleRoot
+    ) public onlyOwner {
+        setupEvent(_token, _price, block.timestamp, block.timestamp + _endTimeFromNow, _merkleRoot);
+    }
+
+    function setupEvent(
+        address _token,
+        uint256 _price,
+        uint256 _startTime,
+        uint256 _endTime,
+        bytes32 _merkleRoot
+    ) public onlyOwner {
+        require(_startTime <= _endTime, "FreePass: Invalid Time");
+        mintEvents[eventIndex].token = _token;
+        mintEvents[eventIndex].price = _price;
+        mintEvents[eventIndex].startTime = _startTime;
+        mintEvents[eventIndex].endTime = _endTime;
+        mintEvents[eventIndex].merkleRoot = _merkleRoot;
+        emit EventCreated(eventIndex, _token, _startTime, _endTime, _merkleRoot);
+        unchecked {
+            ++eventIndex;
+        }
+    }
+
+    function updateMerkleRoot(uint256 _eventIndex, bytes32 _merkleRoot) public onlyOwner {
+        mintEvents[_eventIndex].merkleRoot = _merkleRoot;
+    }
+
+    function withdrawToken(address _token, uint256 _amount) public onlyOwner {
+        IERC20(_token).transfer(msg.sender, _amount);
+    }
+
+    function isMinted(uint256 _eventIndex, address _address) public view returns (bool) {
+        return mintEvents[_eventIndex].mintedAddresses[_address];
+    }
+
+    function freeMint(
+        uint256 _eventIndex,
+        bytes32[] calldata _merkleProof,
+        address _to
+    ) public {
+        MintEvent storage mintEvent = mintEvents[_eventIndex];
+        require(block.timestamp >= mintEvent.startTime, "FreePass: Have not started!");
+        require(block.timestamp <= mintEvent.endTime, "FreePass: Expired!");
+        require(!mintEvent.mintedAddresses[_to], "FreePass: Already minted!");
+
+        bytes32 leaf = keccak256(abi.encodePacked(_to));
+        require(MerkleProof.verify(_merkleProof, mintEvent.merkleRoot, leaf), "FreePass: Unable to verify");
+
+        mintEvent.mintedAddresses[_to] = true;
+        if (mintEvent.price > 0) {
+            IERC20(mintEvent.token).transferFrom(msg.sender, address(this), mintEvent.price);
+        }
+        _safeMint(_to, tokenIndex);
+        unchecked {
+            ++tokenIndex;
+        }
+        emit Minted(_eventIndex, _to);
+    }
+
+    function tokenURI(uint256 tokenId) public pure virtual override returns (string memory) {
+        return "---TestURI---";
+    }
+}
---
+++ test/test.ts
@@ -0,0 +1,171 @@
+import { expect } from "chai";
+import { BigNumber, constants, Signer, utils } from "ethers";
+import { ethers, network } from "hardhat";
+import { MerkleTree } from "merkletreejs";
+import { FreePass, FreePass__factory, MaskToken, MaskToken__factory } from "../types";
+
+const ONE_ETH = utils.parseEther("1");
+
+describe("FreePass", () => {
+  let snapshotId: string;
+  let deployer: Signer;
+  let signer1: Signer;
+  let signer2: Signer;
+  let signer3: Signer;
+  let deployerAddress: string;
+  let signer1Address: string;
+  let signer2Address: string;
+  let signer3Address: string;
+
+  let freePassContract: FreePass;
+  let maskToken: MaskToken;
+
+  let airdropList: string[] = [];
+  let merkleTree: MerkleTree;
+  let merkleRoot: string;
+
+  before(async () => {
+    [deployer, signer1, signer2, signer3] = await ethers.getSigners();
+    deployerAddress = await deployer.getAddress();
+    signer1Address = await signer1.getAddress();
+    signer2Address = await signer2.getAddress();
+    signer3Address = await signer3.getAddress();
+    freePassContract = await new FreePass__factory(deployer).deploy("FreePass", "FP");
+    maskToken = await new MaskToken__factory(deployer).deploy();
+    await maskToken.transfer(freePassContract.address, utils.parseEther("100"));
+    expect(await maskToken.balanceOf(freePassContract.address)).to.be.eq(utils.parseEther("100"));
+
+    airdropList.push(utils.keccak256(signer1Address));
+    airdropList.push(utils.keccak256(signer2Address));
+    airdropList.push(utils.keccak256(signer3Address));
+    merkleTree = new MerkleTree(airdropList, utils.keccak256, { sortPairs: true });
+    merkleRoot = merkleTree.getHexRoot();
+  });
+
+  beforeEach(async () => {
+    snapshotId = await network.provider.send("evm_snapshot", []);
+  });
+
+  afterEach(async () => {
+    await network.provider.send("evm_revert", [snapshotId]);
+  });
+
+  it("Test Owner", async () => {
+    expect(await freePassContract.owner()).to.be.eq(deployerAddress);
+    await expect(freePassContract.connect(signer1).transferOwnership(signer1Address)).to.be.revertedWith(
+      "Ownable: caller is not the owner",
+    );
+    await expect(freePassContract.withdrawToken(maskToken.address, utils.parseEther("101"))).to.be.reverted;
+    await freePassContract.withdrawToken(maskToken.address, utils.parseEther("50"));
+    expect(await maskToken.balanceOf(freePassContract.address)).to.be.eq(utils.parseEther("50"));
+    await freePassContract.withdrawToken(maskToken.address, utils.parseEther("50"));
+    expect(await maskToken.balanceOf(freePassContract.address)).to.be.eq(0);
+  });
+
+  it("Time check", async () => {
+    let leaf = utils.keccak256(signer1Address);
+    let proof = merkleTree.getHexProof(leaf);
+    expect(merkleTree.verify(proof, leaf, merkleRoot)).to.be.true;
+    let index = await freePassContract.eventIndex();
+    expect(index).to.be.eq(BigNumber.from(0));
+
+    await expect(
+      freePassContract["setupEvent(address,uint256,uint256,uint256,bytes32)"](
+        maskToken.address,
+        constants.Zero,
+        constants.MaxUint256,
+        constants.Zero,
+        merkleRoot,
+      ),
+    ).to.be.revertedWith("FreePass: Invalid Time");
+
+    await freePassContract["setupEvent(address,uint256,uint256,uint256,bytes32)"](
+      maskToken.address,
+      constants.Zero,
+      constants.MaxUint256,
+      constants.MaxUint256,
+      merkleRoot,
+    );
+    expect(await freePassContract.eventIndex()).to.be.eq(index.add(1));
+    expect(await freePassContract.balanceOf(signer1Address)).to.be.eq(BigNumber.from(0));
+    await expect(freePassContract.freeMint(index, proof, signer1Address)).to.be.revertedWith(
+      "FreePass: Have not started!",
+    );
+  });
+
+  it("Claim Test", async () => {
+    let leaf = utils.keccak256(signer1Address);
+    let proof = merkleTree.getHexProof(leaf);
+    expect(merkleTree.verify(proof, leaf, merkleRoot)).to.be.true;
+    let index = await freePassContract.eventIndex();
+    expect(index).to.be.eq(BigNumber.from(0));
+    await freePassContract["setupEvent(address,uint256,uint256,bytes32)"](
+      maskToken.address,
+      constants.Zero,
+      BigNumber.from(1000),
+      merkleRoot,
+    );
+    expect(await freePassContract.eventIndex()).to.be.eq(index.add(1));
+    expect(await freePassContract.balanceOf(signer1Address)).to.be.eq(BigNumber.from(0));
+
+    // Fail Case: address not in the whitelist
+    await expect(freePassContract.freeMint(index, proof, deployerAddress)).to.be.revertedWith(
+      "FreePass: Unable to verify",
+    );
+
+    // Success
+    expect(await freePassContract.isMinted(index, signer1Address)).to.be.false;
+    await freePassContract.freeMint(index, proof, signer1Address);
+    expect(await freePassContract.isMinted(index, signer1Address)).to.be.true;
+    expect(await freePassContract.balanceOf(signer1Address)).to.be.eq(1);
+
+    // Fail Case: already claimed
+    await expect(freePassContract.freeMint(index, proof, signer1Address)).to.be.revertedWith(
+      "FreePass: Already minted!",
+    );
+
+    leaf = utils.keccak256(signer2Address);
+    proof = merkleTree.getHexProof(leaf);
+    expect(merkleTree.verify(proof, leaf, merkleRoot)).to.be.true;
+    index = await freePassContract.eventIndex();
+    expect(index).to.be.eq(BigNumber.from(1));
+    await freePassContract["setupEvent(address,uint256,uint256,bytes32)"](
+      maskToken.address,
+      constants.Zero,
+      BigNumber.from(1000),
+      merkleRoot,
+    );
+    expect(await freePassContract.eventIndex()).to.be.eq(index.add(1));
+    expect(await freePassContract.balanceOf(signer2Address)).to.be.eq(BigNumber.from(0));
+
+    // Fail Case: expired
+    await network.provider.send("evm_increaseTime", [1001]);
+    await expect(freePassContract.freeMint(index, proof, signer2Address)).to.be.revertedWith("FreePass: Expired!");
+  });
+
+  it("Update merkle root", async () => {
+    let leaf = utils.keccak256(signer1Address);
+    let index = await freePassContract.eventIndex();
+    let proof = merkleTree.getHexProof(leaf);
+
+    await freePassContract["setupEvent(address,uint256,uint256,bytes32)"](
+      maskToken.address,
+      constants.Zero,
+      BigNumber.from(1000),
+      merkleRoot,
+    );
+
+    const beforeClaimSnapshot = await network.provider.send("evm_snapshot", []);
+    await freePassContract.freeMint(index, proof, signer1Address); // make sure signer1 can claim
+    await network.provider.send("evm_revert", [beforeClaimSnapshot]);
+
+    const newList = airdropList.slice(1, 3); // remove signer1 from the list
+    let tree = new MerkleTree(newList, utils.keccak256, { sortPairs: true });
+    let root = "0x" + tree.getRoot().toString("hex");
+    expect(tree.verify(tree.getProof(leaf), leaf, root)).to.be.false;
+    await freePassContract.updateMerkleRoot(index, root);
+    await expect(freePassContract.freeMint(index, proof, signer1Address)).to.be.revertedWith(
+      "FreePass: Unable to verify",
+    );
+  });
+});
